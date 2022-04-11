8b46d45fadfa2763db9548be7dbbcff43458c1f8
galihwahyuar <wahyuuarofah@gmail.com>
2022-04-11 09:23:45 +0700

    first commit

---
+++ prosesForm_3.php
@@ -0,0 +1,29 @@
+<?php
+    if (isset ($_GET ['nama']) AND isset($_GET['email'])){
+        $nama=$_GET['nama'];
+        $email=$_GET['email'];
+        $komentar=$_GET['komentar'];
+        $isi_form="$nama=$nama&email=$email$komentar=$komentar";
+    }
+    else{
+        header("Location:form_2.php?error=variabel_belum_diset");
+    }
+
+    if(empty($nama)){
+        header("Location:form_2.php?error=nama_kosong".$sisi_form);
+    }
+
+    else if(!preg_match("/^[zA-Z ]*$", $nama)){
+        header("Location:form_2.php?error=nama_kosong".$sisi_form);
+    }
+    else if(empty($email)){
+        header("Location:form_2.php?error=email_kosong".$sisi_form);
+    }
+    else 
+    if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+        header("Location:form_2.php?error=email_kosong".$sisi_form);
+    }
+    else{
+        echo "Name: $nama <br> Email: $email <br> Komentar: $komentar";
+    }
+?>
